9fafc715c70c1847802a4ef6a8b69d97547dae6f
zhupa <zhupa@2345.com>
2020-03-24 18:24:48 +0800

    addTwoNum存疑

--- LinkedList/addTwoNumbers.js
+++ LinkedList/addTwoNumbers.js
@@ -23,3 +23,15 @@ const arrayA = [1,2,3]
 const arrayB =[4,5,6]
 const LinkedListA = new List(arrayA)
 const LinkedListB = new List(arrayB)
+
+var addTwoNumbers = function(l1, l2) {
+  let curr = l1
+  const addRes = l1.val + l2.val
+  curr.val = addRes >= 10 ? addRes -10 : addRes
+  // 相当于后续节点
+  if (!l1 || !l1.next) return curr
+  curr.next = addTwoNumbers(l1.next, l2.next)
+  return curr
+};
+const res = addTwoNumbers(LinkedListA.head, LinkedListB.head)
+console.log(res)
