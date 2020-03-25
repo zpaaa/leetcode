03c10e8729309939f4863917a62cf31e94fdd387
zhupa <zhupa@2345.com>
2020-03-25 11:13:28 +0800

    递归解出两数相加

--- LinkedList/addTwoNumbers.js
+++ LinkedList/addTwoNumbers.js
@@ -19,19 +19,47 @@ function List(array) {
     i++
   }
 }
-const arrayA = [1,2,3]
-const arrayB =[4,5,6]
+const arrayA = [8,9,9]
+const arrayB =[2]
 const LinkedListA = new List(arrayA)
 const LinkedListB = new List(arrayB)
 
 var addTwoNumbers = function(l1, l2) {
-  let curr = l1
+  // console.log('l1--------------', l1)
+  // console.log('l2--------------', l2)
+  if (l1 && l1.val >=10) {
+    l1.val -= 10
+    l1.next ? l1.next.val++ : l1.next = new ListNode(1)
+  }
+  
+  if (l2 && l2.val >= 10) {
+    l2.val -= 10
+    l2.next ? l2.next.val++ : l2.next = new ListNode(1)  
+  }
+
+  // 这时l1、l2 必定都存在
+  let curr
+  if (!l1.next && l2.next) curr = l2
+  else if (!l2.next && l1.next) curr = l1
+  else curr = l1
   const addRes = l1.val + l2.val
-  curr.val = addRes >= 10 ? addRes -10 : addRes
-  // 相当于后续节点
-  if (!l1 || !l1.next) return curr
-  curr.next = addTwoNumbers(l1.next, l2.next)
+  curr.val = addRes >= 10 ? addRes - 10 : addRes
+  // 进位
+  if (addRes >= 10) {
+    if (!l1.next && l2.next) {
+      l2.next.val++
+    } else if (!l2.next && l1.next) {
+      l1.next.val++
+    } else {
+      l1.next ? l1.next.val++ : l1.next = new ListNode(1)
+    }
+  }
+
+  if (!l1.next && !l2.next) return curr
+  curr.next = addTwoNumbers(l1.next || new ListNode(0), l2.next || new ListNode(0))
   return curr
 };
+
+
 const res = addTwoNumbers(LinkedListA.head, LinkedListB.head)
 console.log(res)
